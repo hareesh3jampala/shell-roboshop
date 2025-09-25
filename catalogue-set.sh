4affdeb5eaf089c68ccbad0090b8ace626724ef1
hareesh3jampala <hareesh3jampala@gmail.com>
2025-09-26 00:33:41 +0530

    some message

--- catalogue-set.sh
+++ catalogue-set.sh
@@ -51,7 +51,7 @@ cp $SCRIPT_DIR/catalogue.service /etc/systemd/system/catalogue.service
 systemctl daemon-reload
 systemctl enable catalogue &>>$LOG_FILE
 cp $SCRIPT_DIR/mongo.repo /etc/yum.repos.d/mongo.repo
-dnf install mongodb-mongoshsfds -y &>>$LOG_FILE
+dnf install mongodb-mongosh -y &>>$LOG_FILE
 
 INDEX=$(mongosh mongodb.hareesh3jampala.in --quiet --eval "db.getMongo().getDBNames().indexOf('catalogue')")
 if [ $INDEX -le 0 ]; then
